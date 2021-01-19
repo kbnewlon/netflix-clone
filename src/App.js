17742442940a3d5dec66fba9514cf7b853859d92
Kayla Newlon <kbnewlon@hotmail.com>
2021-01-19 15:16:11 -0800

    axios file baseURL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Row from './component/Row';
+import requests from './requests';
+
 
 function App() {
   return (
     <div className="App">
      <h1>Netflix Clone</h1>
+     <Row title ="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+     <Row title ="Trending Now" fetchUrl={requests.fetchTrending}/>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/component/Row.js
@@ -0,0 +1,23 @@
+import React, { setState, useEffect } from 'react'
+
+function Row ({ title }) {
+
+const [movies, setMovies] = setState([]);
+
+useEffect(() => {
+
+}, []);
+
+    return (
+        <div>
+           <h2>{title}</h2>
+
+            {/* container -> posters */}
+
+
+
+        </div>
+    )
+}
+
+export default Row; 
